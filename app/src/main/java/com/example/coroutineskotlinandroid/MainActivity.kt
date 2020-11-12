ecd12c7c1957d0a642302929f279d70bad365c12
mark <markpackham1@gmail.com>
2020-11-12 17:59:03 +0000

    compared coroutine to normal thread, https://youtu.be/yc_WfBp-PdE?t=9

--- app/src/main/java/com/example/coroutineskotlinandroid/MainActivity.kt
+++ app/src/main/java/com/example/coroutineskotlinandroid/MainActivity.kt
@@ -2,15 +2,24 @@ package com.example.coroutineskotlinandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 // Learn about Coroutines from https://www.youtube.com/watch?v=kvfpuzSwVZ8
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch {  }
+        GlobalScope.launch {
+            delay(3000L)
+            Log.d(TAG, "Coroutines says hello from thread ${Thread.currentThread().name}")
+        }
+        Log.d(TAG, "Hello from thread NOT in Coroutine ${Thread.currentThread().name}")
     }
 }
