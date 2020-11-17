190114e698636b3109a7e27225adeeac7f7c072a
mark <markpackham1@gmail.com>
2020-11-17 16:12:13 +0000

    add runBlocking, https://youtu.be/55W60o9uzVc?t=7

--- app/src/main/java/com/example/coroutineskotlinandroid/MainActivity.kt
+++ app/src/main/java/com/example/coroutineskotlinandroid/MainActivity.kt
@@ -3,9 +3,8 @@ package com.example.coroutineskotlinandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 // Learn about Coroutines from https://www.youtube.com/watch?v=kvfpuzSwVZ8
 class MainActivity : AppCompatActivity() {
@@ -16,10 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch {
-            delay(3000L)
-            Log.d(TAG, "Coroutines says hello from thread ${Thread.currentThread().name}")
+        Log.d(TAG, "Before runBlocking")
+        runBlocking {
+            launch(Dispatchers.IO) {
+                delay(3000L)
+                Log.d(TAG, "Finished IO Coroutine 1")
+            }
+            launch(Dispatchers.IO) {
+                delay(3000L)
+                Log.d(TAG, "Finished IO Coroutine 2")
+            }
+            Log.d(TAG, "Start of runBlocking")
+            delay(5000L)
+            Log.d(TAG, "End of runBlocking")
         }
-        Log.d(TAG, "Hello from thread NOT in Coroutine ${Thread.currentThread().name}")
+        Log.d(TAG, "After runBlocking")
+
     }
+
+
+
 }
